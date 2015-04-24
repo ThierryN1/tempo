2c8154e8632599506224a0b4ba0367dc7608eeb9
unknown <thierryn1@SJCTHIERRYN.verifone.com>
2015-04-24 16:34:36 -0700

    Backup

--- ajax/server.php
+++ ajax/server.php
@@ -1,6 +1,9 @@
 <?php
 
-$arr = array('firstName' => 'John', 'lastName' => 'Smith');
+if($_GET["id"]=="1")
+	$arr = array('firstName' => 'John', 'lastName' => 'Smith');
+else 
+	$arr = array('firstName' => 'Betty', 'lastName' => 'Johnson');
 echo json_encode($arr);
 
 ?>
